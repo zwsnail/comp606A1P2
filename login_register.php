227f36c3a85fbe1adc41d4d2280d891cd7ab6e23
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-09-18 23:04:46 +1200

    Update login_register.php
    add validation rules

--- login_register.php
+++ login_register.php
@@ -6,32 +6,34 @@
 	<meta name="viewport" content="width=device-wideth,initial-scale=1">
 	<title>User Registration & Login System</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonyous">
-	<link href="css/style.css" type="text/css" rel="stylesheet" media="all">
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 	<style type="text/css">
 		#alert, #register-box, #forgot-box{
 			display: none;
 		}
 	</style>
+	
 </head>
-
-<body>  
+<body class="bg-dark">
 	<div class="container mt-4">
-		<!-- <div class="row">
+		<div class="row">
 			<div class="col-lg-4 offset-lg-4" id="alert">
 				<div class="alert alert-success">
 					<strong id="result">Hello World!</strong>
 				</div>
 			</div>
-		</div> -->
-
+		</div>
+ 
 		<!-- login form -->
-
-		<div class="row">
+    <div class = "form_area">
+		<div class="row" >
 			<div class="col-lg-4 offset-lg-4 bg-light rounded" id="login-box">
 				<h2 class="text-center mt-2">Login</h2>
-				<form action="" method="post" role="form" class="p-2" id="login-frm">
+				<form action="login_here.php" method="post" role="form" class="p-2" id="login-frm">
 					<div class="form-group">
-						<input type="text" name="username" class="form-control" placeholder="Username" required minlength="3">
+						<input type="email" name="email" class="form-control" placeholder="Email" required>
 					</div>
 					<div class="form-group">
 						<input type="password" name="password" class="form-control" placeholder="Password" required minlength="6">
@@ -63,9 +65,9 @@
 		<div class="row">
 			<div class="col-lg-4 offset-lg-4 bg-light rounded" id="register-box">
 				<h2 class="text-center mt-2">Register</h2>
-				<form action="" method="post" role="form" class="p-2" id="register-frm">
+				<form action="insert_data.php" method="post" role="form" class="p-2" id="register-frm">
 					<div class="form-group">
-						<input type="text" name="name" class="form-control" placeholder="Fullname" required minlength="3">
+						<input type="text" name="fullname" class="form-control" placeholder="Fullname" required minlength="3">
 					</div>
 					<div class="form-group">
 					<label><input type="radio" name="gender" class=".radio-inline" value="female" required>Female</label>
@@ -73,25 +75,27 @@
 					</div>
 
 					<div class="form-group">
-						<label>Date of Birthday<input type="date" class="form-control" value="date" required></label>
+						<label>Date of Birthday<input type="date" class="form-control" name="birth_date" value="date" required></label>
 					</div>
-
-					<div class="form-group">
-						<input type="email" name="email" class="form-control" placeholder="Email" required>
+                    <div class="form-group">
+						<input type="text" name="mob_number" class="form-control" placeholder="Mobile number">
 					</div>
 					<div class="form-group">
-						<label>Which time would you like to come?<input type="datetime-local" name="appointTime" class="form-control" required></label>
+						<input type="email" name="email" class="form-control" placeholder="Email" id="email" required>
+					</div>
+					<div class="form-group">
+						<label>Which time would you like to come?<input type="datetime-local" name="massage_time" class="form-control" required></label>
 					</div>
 
 					<div class="form-group">
 
 
 				      <label for="sel">May I know the reason you want to have the massage?(hold shift to select more than one)</label>
-				      <select multiple class="form-control" id="sel">
-				        <option name="motivation" value="Recovering from injury">Recovering from injury</option>
-				        <option name="motivation" value="Dealing with anxiety">Dealing with anxiety</option>
-				        <option name="motivation" value="Just relaxing">Just relaxing</option>
-				        <option name="motivation"value="Other reason I don't want to say" >Other reason I don't want to say</option>
+				      <select multiple class="form-control" name="reason" id="sel">
+				        <option name="reason" value="Recovering from injury">Recovering from injury</option>
+				        <option name="reason" value="Dealing with anxiety">Dealing with anxiety</option>
+				        <option name="reason" value="Just relaxing">Just relaxing</option>
+				        <option name="reason"value="Other reason I don't want to say" >Other reason I don't want to say</option>
 				      </select>
 
 					</div>
@@ -99,12 +103,12 @@
 
 					<div class="form-group">
 						<label>Please choose the package</label>
-						<div class="custom-control custom-radio mb-3">
-					       <input type="radio" class="custom-control-input" id="price1" name="price" value="45">
+						<div class="custom-control custom-checkbox mb-3">
+					       <input type="checkbox" class="custom-control-input" id="price1" name="price" value="45">
 					       <label class="custom-control-label" for="price1">$45 for 30min</label>
 					    </div>
-						<div class="custom-control custom-radio mb-3">
-					       <input type="radio" class="custom-control-input" id="price2" name="price" value="75">
+						<div class="custom-control custom-checkbox mb-3">
+					       <input type="checkbox" class="custom-control-input" id="price2" name="price" value="75">
 					       <label class="custom-control-label" for="price2">$75 for 60min</label>
 				    	</div>
 				    </div>
@@ -117,7 +121,7 @@
 					</div> -->
 
 					<div class="form-group">
-						<input type="password" name="pass" id="pass"class="form-control" placeholder="Password" required minlength="6">
+						<input type="password" name="password" id="pass"class="form-control" placeholder="Password" required minlength="6">
 					</div>
 					<div class="form-group">
 						<input type="password" name="cpass" id="cpass" class="form-control" placeholder="Comfirm Password" required minlength="6">
@@ -125,7 +129,7 @@
 					<div class="form-group">
 						<div class="custom-control custom-checkbox">
 							<input type="checkbox" name="rem" class="custom-control-input" id="customCheck2">
-							<label for="customCheck2" class="custom-control-label">I green to the <a id="term" href="#">terms & conditions.</a></label>
+							<label for="customCheck2" class="custom-control-label">I green to the <a href="#">terms & conditions.</a></label>
 						</div>
 					</div>
 					<div class="form-group">
@@ -148,10 +152,10 @@
 		<div class="row">
 			<div class="col-lg-4 offset-lg-4 bg-light rounded" id="forgot-box">
 				<h2 class="text-center mt-2">Reset Password</h2>
-				<form action="" method="post" role="form" class="p-2" id="forgot-frm">
+				<form action="reset_password.php" method="post" role="form" class="p-2" id="forgot-frm">
 					<div class="form-group">
 						<small class="text-muted">
-							To reset your password, enter the email address, new password.
+							To reset your password, enter the email address and we will send reset password instructions on your email.
 						</small>
 
 					</div>
@@ -159,7 +163,10 @@
 						<input type="email" name="femail" class="form-control" placeholder="Email" required>
 					</div>
 					<div class="form-group">
-						<input type="text" name="newpassword" class="form-control" placeholder="New Password" required>
+						<input type="password" name="password" id="rpass"class="form-control" placeholder="Password" required minlength="6">
+					</div>
+					<div class="form-group">
+						<input type="password" name="cpass" id="rcpass" class="form-control" placeholder="Comfirm Password" required minlength="6">
 					</div>
 					<div class="form-group">
 						<input type="submit" name="forgot" id="forgot" value="Reset" class="btn btn-primary btn-block">
@@ -175,14 +182,14 @@
 			</div>
 		</div>
 
-
+</div>
 
 	</div>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" crossorigin="anonyous"></script>
 	<script src="http://code.jquery.com/jquery-3.4.1.min.js"   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="   crossorigin="anonymous"></script>
 
   	<!-- form validation by using jQuery -->
-
+    
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
 	<script>
 		// hide different form by usage
@@ -210,16 +217,32 @@
 		   		rules:{
 		   			cpass:{
 		   				equalTo:"#pass",
+		   			},
+		   		email: {
+            required: true,
+            email: true,
+                remote: {
+                    url: "check_email.php",
+                    type: "post"
+                 }
+        }
+    },
+    messages: {
+        email: {
+            required: "Please enter your email address.",
+            email: "Please enter a valid email address.",
+            remote: "Email already in use!"
+        }
+    }
+	});
+		   	$("#forgot-frm").validate({
+		   		rules:{
+		   			rcpass:{
+		   				equalTo:"#rpass",
 		   			}
-		   		}
-		   	});
-		   	$("#forgot-frm").validate();
+			   }
 		});
-
-		// click the terms to show the cancellation fees
-		$("#term").click(function(){
-			alert("Please notice! If you cancel the booking with 24 hours leading up to the appointment, you will be charged $20 late cancellation fee");
-		  });
+		});
 	</script>
 
 
