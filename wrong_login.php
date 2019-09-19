4c67e2630112c911165c5217500041e1feb2bcf7
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-09-19 23:26:26 +1200

    Add files via upload
    session out and login validation

---
+++ wrong_login.php
@@ -0,0 +1,19 @@
+<?php
+// include("dbh.inc.connection.php");
+
+?>
+<style>
+ body {
+    background-image: url(images/sitedown.jpg);
+    background-size: cover;
+}
+</style>
+<h1 style="text-align:center;">Try Again</h1>
+<div class='form'>
+<h3>Username/password is incorrect.</h3>
+<br/>Click here to <a href='login_register.php'>Login</a></div>
+
+
+<?php 
+// require_once("footer.php"); 
+?> 
