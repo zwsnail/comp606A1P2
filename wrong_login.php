aa93c8af361286ce2593a28731eb68f0cba012ca
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-09-23 13:51:21 +1200

    Update wrong_login.php
    add style

--- wrong_login.php
+++ wrong_login.php
@@ -1,18 +1,22 @@
-<?php
-// include("dbh.inc.connection.php");
-
-?>
+<?php include_once("header.php"); ?>
 <style>
  body {
     background-image: url(images/sitedown.jpg);
     background-size: cover;
 }
+.div_area {
+    padding: 150px;
+    font-size: 17px;
+    color: #000;
+    font-weight: 600;
+}
 </style>
+<div style="text-align:center;" class="div_area">
 <h1 style="text-align:center;">Try Again</h1>
 <div class='form'>
 <h3>Username/password is incorrect.</h3>
 <br/>Click here to <a href='login_register.php'>Login</a></div>
-
+<div>
 
 <?php 
 // require_once("footer.php"); 
