2efca6707db162695c21cd3a18c0d0d3196b8833
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-09-23 23:06:10 +1200

    Update update_booking.php
    update fields

--- update_booking.php
+++ update_booking.php
@@ -7,6 +7,10 @@ if (isset($_POST['massage_time'])){   //check if fullname is there
   mysqli_query() function is used to execute SQL queries. */
   $massage_time = stripslashes($_POST['massage_time']);
   $massage_time = mysqli_real_escape_string($con,$massage_time); 
+  $time = stripslashes($_POST['time']);
+  $time = mysqli_real_escape_string($con,$time); 
+  $massage_type = stripslashes($_POST['massage_type']);
+  $massage_type = mysqli_real_escape_string($con,$massage_type); 
   $reason = stripslashes($_POST['reason']);
    $reason = mysqli_real_escape_string($con,$reason); 
    $price = stripslashes($_POST['price']);
@@ -15,7 +19,7 @@ if (isset($_POST['massage_time'])){   //check if fullname is there
    $booking_id = mysqli_real_escape_string($con,$booking_id); 
    $updated = date("Y-m-d H:i:s");
    // query to update data
-        $query = "UPDATE booking SET massage_time='$massage_time',reason='$reason',price='$price',updated='$updated' WHERE id='$booking_id'";
+        $query = "UPDATE booking SET massage_time='$massage_time',time='$time',massage_type='$massage_type',reason='$reason',price='$price',updated='$updated' WHERE id='$booking_id'";
         //echo $query;
         
         
